2465f214714c3d7101626e31874a8c37b9239a41
JunHo Kim <ghcjswo@gmail.com>
2021-07-26 16:44:27 +0900

    add ivtc process. refine log, comment

--- pillar_detection.py
+++ pillar_detection.py
@@ -1,6 +1,3 @@
-import io
-from contextlib import redirect_stderr
-
 import os
 from collections import Counter
 import cv2
@@ -9,14 +6,16 @@ import re
 import subprocess
 import argparse
 import copy
-import base64
+import time
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--input_dir", default="./", help="Input vid directory")
+parser.add_argument("-s", "--source_dir", default="./", help="Input source vid directory")
+parser.add_argument("-i", "--ivtc_dir", default="./ivtc", help="Input ivtc vid directory")
 parser.add_argument("-o", "--output_dir", default="./out", help="Output vid directory")
 parser.add_argument("-d", "--debug", default=False, action="store_true", help="for debugging. short output")
 args = parser.parse_args()
 
+# Adjust crop info in display layout.
 def resizeCropInfo(key, cropPos, frame_width, frame_height):
     def _IsOutOfBound(_cropPos):
         w, h, x, y = _cropPos
@@ -69,27 +68,62 @@ def resizeCropInfo(key, cropPos, frame_width, frame_height):
 
 def main():
     vid_types = ('*.avi', '*.mov', '*.mp4', '*.mxf')
-    vid_grapped = []
+    source_dir = args.source_dir
+    ivtc_dir = args.ivtc_dir
+    output_dir = args.output_dir
+    
+    source_list = []
+    ivtc_list = []
     for types in vid_types :
-        vid_grapped += sorted(glob.glob(os.path.join(args.input_dir, types)))
+        source_list += sorted(glob.glob(os.path.join(source_dir, types)))
 
-    #--------------------------------------------------------
-    f = io.StringIO()
-    #--------------------------------------------------------
+    ivtc_list += sorted(glob.glob(os.path.join(ivtc_dir, "*.mp4")))
 
-    for i, fpath in enumerate(vid_grapped):
-        # debugging
-        # fpath = "01. TRAX - PARADOX MV.avi"
+    if len(ivtc_list) > 0:
+        ivtc_input_list = []
+        for i, fpath in enumerate(source_list):
+            vid_name = os.path.basename(fpath)[:-4]
+            ivtc_name_list = [os.path.basename(x)[:-4] for x in ivtc_list]
 
-        if i >= 68 :
-            return
+            if not vid_name + "_ivtc" in ivtc_name_list :
+                ivtc_input_list += glob.glob(fpath)
+                print(f"[INFO]\t Seems \"{os.path.basename(fpath)}\" is stil telecine... add to ivtc processing list")
+    else :
+        ivtc_input_list = source_list
 
+    for i, ivtc_input in enumerate(ivtc_input_list) :
+        ivtc_input_num = len(ivtc_input_list)
+        ivtc_input_name = os.path.basename(ivtc_input)
+        ivtc_output_path = os.path.join(ivtc_dir, ivtc_input_name[:-4]) + "_ivtc.mp4"
+
+        if not os.path.exists(ivtc_dir) :
+            os.mkdir(ivtc_dir)
+            print(f'[INFO]\t {ivtc_dir} created')
+        
+        print(f"[INFO]\t Work ivtc on \"{ivtc_input_name}\" ===> \"{ivtc_output_path}\" ... [{i + 1}/{ivtc_input_num}]")
+        print("[INFO]\t Please wait...")
+        start_time = time.time()
+        subprocess.run(["ffmpeg", "-ss", "10", "-i", ivtc_input, "-vf", "fieldmatch=mode=pc_n:combmatch=full,bwdif=0:-1:1,decimate", "-c:v", "libx264", "-crf", "0",
+                        "-preset", "ultrafast","-loglevel", "quiet", "-y", ivtc_output_path])
+        t = time.time() - start_time
+        print("[INFO]\t Work done. [It takes %.2f seconds.]" % t)
+                    
+
+    ivtc_list = []
+    ivtc_list += sorted(glob.glob(os.path.join(ivtc_dir, "*.mp4")))
+    target_ratio = 16 / 9
+    print("[INFO]\t Target ratio : %.7f" %target_ratio)
+    print("[INFO]\t Input ivtc dir :", ivtc_dir)
+    for i, fpath in enumerate(ivtc_list):
         vid_name = os.path.basename(fpath)[:-4]
-        vid_num = len(vid_grapped)
-        # vid_name = vid_name.encode('utf-8').decode('cp949')
+        vid_num = len(ivtc_list)
 
-        print(f"Video num : {i + 1} / {vid_num}")
-        print("File to detect crop: ", fpath)
+        # Korean title text bug fix (work in progress)
+        # vid_name = vid_name.encode('utf-8').decode('cp949')
+        
+        print(f"[INFO]\t Video num : {i + 1} / {vid_num}")
+        print("[INFO]\t File to detect crop area: ", fpath)
+        print("[INFO]\t Detecting crop area...")
         p = subprocess.Popen(["ffmpeg", "-ss", "10", "-i", fpath, "-vf", "cropdetect=limit=38",
                              "-vframes", "3000", "-f", "null", "out.null"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         infos = p.stderr.read().decode('utf-8')
@@ -98,116 +132,119 @@ def main():
         # print ("Crop Infos : ")
         # print (allCrops)
         mostCommonCrop = Counter(allCrops).most_common(1)
-        print("most common detected crop info: %s" % mostCommonCrop)
-        global crop 
-        crop = mostCommonCrop[0][0]
 
-        # Delete "crop=" in list cropPos
+        if len(mostCommonCrop) > 0 :
+            crop = mostCommonCrop[0][0]
+        else :
+            print(f"[!!! ERROR !!!]\t Can`t read input vid. Skip to next vid \t ({fpath})")
+            continue
+
+        # Delete "crop=" in list "crop"
         cropPos = crop.split(":")
         cropPos[0] = cropPos[0][5:]
+        print(f"[INFO]\t Most common detected crop area: [w : {cropPos[0]}, h : {cropPos[1]}, x : {cropPos[2]}, y : {cropPos[3]}]")
         cropPos = [int(x) for x in cropPos]
 
-        with redirect_stderr(f):
-            # Capture vid, Get Vid info
-            capture = cv2.VideoCapture(fpath)
-            frame_width = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
-            frame_height = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
-            fps = capture.get(cv2.CAP_PROP_FPS)
-            frame_pos = 0
-            frame_count = 0
-            target_ratio = 16 / 9
-            
-            # Crop Area even num check
-            if (cropPos[0] % 2) != 0 :
-                if (cropPos[0] + 1) >= frame_width:
-                    cropPos[0] -= 1
-                else:
-                    cropPos[0] += 1
-            if (cropPos[1] % 2) != 0 :
-                if (cropPos[1] + 1) >= frame_width:
-                    cropPos[1] -= 1
-                else:
-                    cropPos[1] += 1
+        # Capture vid, Get Vid info
+        capture = cv2.VideoCapture(fpath)
+        frame_width = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+        frame_height = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        fps = capture.get(cv2.CAP_PROP_FPS)
+        frame_pos = 0
+        frame_count = 0
+        
+        # Crop Area even num check
+        if (cropPos[0] % 2) != 0 :
+            if (cropPos[0] + 1) >= frame_width:
+                cropPos[0] -= 1
+            else:
+                cropPos[0] += 1
+        if (cropPos[1] % 2) != 0 :
+            if (cropPos[1] + 1) >= frame_width:
+                cropPos[1] -= 1
+            else:
+                cropPos[1] += 1
 
-            # Crop Area Multiple of 4 check
-            if (cropPos[0] % 4) != 0 :
-                if (cropPos[0] + 2) >= frame_width:
-                    cropPos[0] -= 2
-                else:
-                    cropPos[0] += 2
-            if (cropPos[1] % 4) != 0 :
-                if (cropPos[1] + 2) >= frame_width:
-                    cropPos[1] -= 2
-                else:
-                    cropPos[1] += 2
-    
-            print("Target Ratio : %.7f" %target_ratio)
-            print("Input dir :", args.input_dir)
+        # Crop Area Multiple of 4 check
+        if (cropPos[0] % 4) != 0 :
+            if (cropPos[0] + 2) >= frame_width:
+                cropPos[0] -= 2
+            else:
+                cropPos[0] += 2
+        if (cropPos[1] % 4) != 0 :
+            if (cropPos[1] + 2) >= frame_width:
+                cropPos[1] -= 2
+            else:
+                cropPos[1] += 2
 
-            key = None
-            isPaused = False
-            pausedFrame = None
-            frame = None
-            cv2.namedWindow(f'{vid_name}', cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)
-            while True:
-                key = cv2.waitKeyEx(10)
+        key = None
+        isPaused = False
+        pausedFrame = None
+        frame = None
+        cv2.namedWindow(f'{vid_name}', cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)
+        while True:
+            key = cv2.waitKeyEx(10)
 
-                # Handle close button event
-                if cv2.getWindowProperty(f'{vid_name}', cv2.WND_PROP_VISIBLE) < 1 :
-                    return
+            # Close button(X button) event
+            if cv2.getWindowProperty(f'{vid_name}', cv2.WND_PROP_VISIBLE) < 1 :
+                return
 
-                if key > 0 :
-                    # 13 == Enter key ASCII Code
-                    if key == 13:
-                        break
-                    # Pause
-                    elif key == ord('p') or key == ord('P'):
-                        if isPaused :
-                            isPaused = False
-                        else :
-                            isPaused = True
-                            frame = copy.deepcopy(pausedFrame)
-                    # Replay
-                    elif key == ord('r') or key == ord('R'):
-                        if not isPaused:
-                            capture = cv2.VideoCapture(fpath)
-                            continue
-                    # Move Forward (10s)
-                    elif key == ord('e') or key == ord('E'):
-                        if not isPaused:
-                            capture.set(cv2.CAP_PROP_POS_FRAMES, frame_pos + (fps * 10))
-                    # Move Backward (10s)
-                    elif key == ord('q') or key == ord('Q'):
-                        if not isPaused:
-                            capture.set(cv2.CAP_PROP_POS_FRAMES, frame_pos - (fps * 10))
-                    # Skip this Vid
-                    elif key == ord('n') or key == ord('N'):
-                        break;
-                    else:
-                        # Crop Area control
-                        cropPos = resizeCropInfo(key, cropPos, frame_width, frame_height)
-
-                frame_pos = capture.get(cv2.CAP_PROP_POS_FRAMES)
-                frame_count = capture.get(cv2.CAP_PROP_FRAME_COUNT)
-                
-                if(frame_pos >= frame_count):
+            if key > 0 :
+                # 13 == Enter key ASCII Code
+                if key == 13:
                     break
-                
-                print("Current Crop Info : ", end="")
-                print(cropPos, end="")
-                print(f"\t Frame Count ({frame_pos}/{frame_count})", end="\r")
+                # Pause
+                elif key == ord('p') or key == ord('P'):
+                    if isPaused :
+                        isPaused = False
+                    else :
+                        isPaused = True
+                        frame = copy.deepcopy(pausedFrame)
+                # Replay
+                elif key == ord('r') or key == ord('R'):
+                    if not isPaused:
+                        capture = cv2.VideoCapture(fpath)
+                        continue
+                # Move Forward (10s)
+                elif key == ord('e') or key == ord('E'):
+                    if not isPaused:
+                        capture.set(cv2.CAP_PROP_POS_FRAMES, frame_pos + (fps * 10))
+                # Move Backward (10s)
+                elif key == ord('q') or key == ord('Q'):
+                    if not isPaused:
+                        capture.set(cv2.CAP_PROP_POS_FRAMES, frame_pos - (fps * 10))
+                elif key == ord('h') or key == ord('H'):
+                    print("[INFO]\t ### KEY GUIDE ########################################################################################################")
+                    print("[INFO]\t #  (MOVE : wasd), (RESIZE : ijkl), (FOR / BACKWARD : q, e), (RESET : r), (PAUSE : p), (SKIP : n), (CONFIRM : enter)  #")
+                    print("[INFO]\t ###################################################################################################################### ")
+                # Skip this Vid
+                elif key == ord('n') or key == ord('N'):
+                    break;
+                else:
+                    # Crop Area control
+                    cropPos = resizeCropInfo(key, cropPos, frame_width, frame_height)
 
-                if not isPaused:
-                    ret, frame = capture.read()
-                    pausedFrame = copy.deepcopy(frame)
-                    cv2.rectangle(frame, (cropPos[2], cropPos[3]), (cropPos[2] + cropPos[0],
-                            cropPos[3] + cropPos[1]), (0, 0, 255), thickness=1, lineType=cv2.LINE_8)    
-                    cv2.imshow(f'{vid_name}', frame)    
-                else :
-                    pausedFrame = copy.deepcopy(frame)
-                    cv2.rectangle(pausedFrame, (cropPos[2], cropPos[3]), (cropPos[2] + cropPos[0],
-                            cropPos[3] + cropPos[1]), (0, 0, 255), thickness=1, lineType=cv2.LINE_8)
-                    cv2.imshow(f'{vid_name}', pausedFrame)
+            frame_pos = capture.get(cv2.CAP_PROP_POS_FRAMES)
+            frame_count = capture.get(cv2.CAP_PROP_FRAME_COUNT)
+            
+            if(frame_pos >= frame_count):
+                break
+
+            print("[INFO]\t Current crop area : ", end="")
+            print(cropPos, end="")
+            print(f"\t Frame Count ({frame_pos}/{frame_count})\t[PRESS \"h\" TO VIEW KEY GUIDE]", end="\r")
+
+            if not isPaused:
+                ret, frame = capture.read()
+                pausedFrame = copy.deepcopy(frame)
+                cv2.rectangle(frame, (cropPos[2], cropPos[3]), (cropPos[2] + cropPos[0],
+                        cropPos[3] + cropPos[1]), (0, 0, 255), thickness=1, lineType=cv2.LINE_8)    
+                cv2.imshow(f'{vid_name}', frame)    
+            else :
+                pausedFrame = copy.deepcopy(frame)
+                cv2.rectangle(pausedFrame, (cropPos[2], cropPos[3]), (cropPos[2] + cropPos[0],
+                        cropPos[3] + cropPos[1]), (0, 0, 255), thickness=1, lineType=cv2.LINE_8)
+                cv2.imshow(f'{vid_name}', pausedFrame)
                 
         print("")
         capture.release()
@@ -222,13 +259,13 @@ def main():
 
         # ffmpeg filter args
         crop_arg = f"crop={cropPos[0]}:{cropPos[1]}:{cropPos[2]}:{cropPos[3]},"
-        print(f"[CropInfo] (W : {cropPos[0]}), (H : {cropPos[1]}), (X : {cropPos[2]}), (Y : {cropPos[3]})")
+        print(f"[INFO]\t [CropInfo] (W : {cropPos[0]}), (H : {cropPos[1]}), (X : {cropPos[2]}), (Y : {cropPos[3]})")
 
         crop_ratio = cropPos[0] / cropPos[1]
         scale_arg = ""
 
-        # Set padding argument
-        # Offset for 16:9 display ratio
+        # Set padding(letter box) argument
+        # variable "offset" for 16:9 output display ratio
         if target_ratio < crop_ratio:
             offset = (9 * cropPos[0] - 16 * cropPos[1]) / 16
             tmp_height = cropPos[1] + offset
@@ -237,7 +274,7 @@ def main():
                 tmp_height -= (tmp_height % 4)
             # scale_arg = f"scale=w={cropPos[0]}:h={tmp_height},"
             pad_arg = f"pad={cropPos[0]}:{tmp_height}:(ow-iw)/2:(ih-oh)/2"
-            print(f"[PadInfo] (W : {cropPos[0]}), (H : {tmp_height})")
+            print(f"[INFO]\t [PadInfo] (W : {cropPos[0]}), (H : {tmp_height})")
         else:
             offset = (16 * cropPos[1] - 9 * cropPos[0]) / 9
             tmp_width = cropPos[0] + offset
@@ -246,15 +283,15 @@ def main():
                 tmp_width -= (tmp_width % 4)
             # scale_arg = f"scale=w={tmp_width}:h={cropPos[1]},"
             pad_arg = f"pad={tmp_width}:{cropPos[1]}:(ow-iw)/2:(ih-oh)/2"
-            print(f"[PadInfo] (W : {tmp_width}), (H : {cropPos[1]})")
+            print(f"[INFO]\t [PadInfo] (W : {tmp_width}), (H : {cropPos[1]})")
 
-        print("Encoding \"%s\" ..." % vid_name)
+        print("[INFO]\t Encoding \"%s\" ..." % vid_name)
 
-        if not os.path.exists(args.output_dir) :
-            os.mkdir(args.output_dir)
-            print(f'{args.output_dir} created')
+        if not os.path.exists(output_dir) :
+            os.mkdir(output_dir)
+            print(f'[INFO]\t {output_dir} created')
 
-        output_path = os.path.join(args.output_dir, f"{vid_name}_box.mp4")
+        output_path = os.path.join(output_dir, f"{vid_name}_box.mp4")
         if args.debug:
             # for Debug
             p = subprocess.Popen(["ffmpeg", "-t", "15", "-ss", "30", "-i", fpath, "-vf", crop_arg + scale_arg + pad_arg, "-y",
